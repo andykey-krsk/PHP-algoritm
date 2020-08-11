33faeb89c2f9e977e23c35d810112fb5fe075662
andykey-krsk <andykey@bk.ru>
2020-08-12 00:16:03 +0700

    Задания к уроку №1. Делитель числа

--- Lesson_1/Task_2.php
+++ Lesson_1/Task_2.php
@@ -2,16 +2,43 @@
 
 function divider($number)
 {
-    $divider = (int)sqrt($number);
-    if ($number % $divider === 0) {
-
+    $max = (int)sqrt($number); //определяем верхиний предел поиска
+    for ($i = $max; $i > 1; $i--) { //перебор от верхнего предела вниз
+        if (($number % $i === 0)) { //проверка делится ли нацело
+            if (isSimple($i)) { //проверка является ли простым
+                return $i;
+            }
+        }
+        if ($i === 2){
+            return "Число простое \n";
+        }
     }
-    return $result;
+}
+
+function isSimple($number)
+{
+    $countDev = 0;
+    $i = 2;
+    while (($i <= $number) && ($countDev <= 1)) {
+        if (($number % $i) === 0) {
+            $countDev++;
+        }
+        $i++;
+    }
+    if ($countDev <= 1) {
+        return true; //простое
+    } else {
+        return false; //составное
+    }
 }
 
 $number1 = 13195;
 $number2 = 600851475143;
 
 echo divider($number1); //29
+echo "\n";
 
 echo divider($number2);
+echo "\n";
+
+echo divider(6857);
