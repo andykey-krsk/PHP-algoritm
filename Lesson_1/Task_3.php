97fcf8cb94f73f589d554a23633308538059f009
andykey-krsk <andykey@bk.ru>
2020-08-12 22:08:42 +0700

    Задания к уроку №1. начало задания 3

--- Lesson_1/Task_3.php
+++ Lesson_1/Task_3.php
@@ -1,6 +1,11 @@
 <?php
+if ($_GET['path'] != '') {
+    $path = $_GET['path'];
+}else{
+    $path = '/';
+}
 $dir = new DirectoryIterator($path);
 
-foreach ($dir as $item){
-    echo $item . PHP_EOL;
+foreach ($dir as $item) {
+    echo "<p><a href='/?path=". $item ."'>" . $item . "</a></p>" . PHP_EOL;
 }
