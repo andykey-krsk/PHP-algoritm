25d45bb8498a4834191f171b00dbddcc213df724
andykey-krsk <andykey@bk.ru>
2020-08-14 00:30:36 +0700

    Задания к уроку №1. fix

--- Lesson_1/Task_3.php
+++ Lesson_1/Task_3.php
@@ -6,11 +6,14 @@ if (empty($_GET['path'])) {
     $path = $_GET['path'];
 }
 
-$dir = new DirectoryIterator($path);
+$dir = new DirectoryIterator(realpath($path));
+
+
 
 foreach ($dir as $item) {
+    if ($item->isDot()) continue;
     if ($item->isDir()) {
-        echo "<p>DIR <a href='/?path=" . $item->getPathname() . "'>" . $item . "</a></p>";
+        echo "<p>DIR <a href='?path=" . $item->getPathname() . "'>" . $item . "</a></p>";
     } else {
         echo "<p>FILE " . $item->getBasename() . "</p>";
     }
