54f0a4b202c55612e3177eec3d7c6b9fbcbae597
andykey-krsk <andykey@bk.ru>
2020-08-26 18:18:57 +0700

    Задания к уроку №3. задание 3 не доделал

--- Lesson_3/Task_3.php
+++ Lesson_3/Task_3.php
@@ -6,41 +6,37 @@
 [4, 4, 4, 4, ...(n)..., 4]
 */
 
-function findNumberCount($array, $number){
-    $count = 0;
-
-    return $count;
-}
-
-$arr1 = [1,1,1,2,3,4,5,6,7,8,8,9,10];
-$num1 = 1;
-$arr2 = [1,2,2,2,2,3,4,5,6,7,8,8,9,10];
-$num2 =2;
-$arr3 = [1,1,1,2,3,3,3,3,4,5,6,7,8,8,9,10];;
-$num3 =3;
-
-echo findNumberCount($arr1,$num1);
-echo '<br><br>';
-
-echo findNumberCount($arr2,$num2);
-echo '<br><br>';
-
-echo findNumberCount($arr3,$num3);
-echo '<br><br>';
-
-function binarySearch($myArray, $num)
+function findNumberCount(&$array, $number)
 {
     $start = 0;
-    $end = count($myArray) - 1;
-    $n = 0;
+    $end = count($array) - 1;
+    $index = null; //индекс найденного
+    $left = null; //индекс крайнего левого найденного
+    $right = null; //индекс крайнего правого найденного
+    while ($start < $end) {
+        $index = binarySearch($array, $number, $start, $end);
+        if ($index) { //что то нашел
+            if ($left === null) {
+                $left = $index;
+            }
+            if ($right === null) {
+                $right = $index;
+            }
+            $end = $index - 1;
+        } else {
+            echo "ЧИСЛО НЕ НАЙДЕНО";
+        }
+    }
+    return $right - $left + 1;
+}
+
+function binarySearch(&$myArray, $num, $start, $end)
+{
+    if ($start == $end) return $start;
 
     while ($start < $end) {
-        $n++;
         $base = floor(($start + $end) / 2);
-        echo "Проверяется элемент с индексом: $base" . PHP_EOL;
-
         if ($myArray[$base] == $num) {
-            echo "Количество итераций: $n искомого числа $num под индексом $base " . PHP_EOL;
             return $base;
         } elseif ($myArray[$base] < $num) {
             $start = $base + 1;
@@ -48,6 +44,25 @@ function binarySearch($myArray, $num)
             $end = $base - 1;
         }
     }
-    echo "ЧИСЛО НЕ НАйДЕНО. Количество итераций: $n" . PHP_EOL;
+    echo "ЧИСЛО НЕ НАЙДЕНО." . PHP_EOL;
     return null;
 }
+
+$arr1 = [1, 1, 1, 2, 3, 4, 5, 6, 7, 8, 8, 9, 10];
+$num1 = 1;
+$arr2 = [1, 2, 2, 2, 2, 3, 4, 5, 6, 7, 8, 8, 9, 10];
+$num2 = 2;
+$arr3 = [1, 1, 1, 2, 3, 3, 3, 3, 4, 5, 6, 7, 8, 8, 9, 10];;
+$num3 = 5;
+
+echo findNumberCount($arr1, $num1);
+echo '<br><br>';
+
+echo findNumberCount($arr2, $num2);
+echo '<br><br>';
+
+echo findNumberCount($arr3, $num3);
+echo '<br><br>';
+
+echo binarySearch($arr3, 5, 0, 15);
+echo '<br><br>';
